03b89844633bf4610cb535025b113606a066d451
Jon Clarke <jonclarke1984@googlemail.com>
2022-03-09 16:27:01 +0000

    reading time done, halfway through find_best_entry_for_reading_time

--- Test Drive A Class System/Exercise/lib/diary.rb
+++ Test Drive A Class System/Exercise/lib/diary.rb
@@ -1,3 +1,5 @@
+require 'diary_entry'
+
 class Diary
   def initialize
     @diary_entries = []
@@ -26,6 +28,9 @@ class Diary
                         # the number of words the user can read per minute
     # Returns an integer representing an estimate of the reading time in minutes
     # if the user were to read all entries in the diary.
+
+    (count_words.to_f / wpm).ceil
+
   end
 
   def find_best_entry_for_reading_time(wpm, minutes)
@@ -36,5 +41,19 @@ class Diary
     # Returns an instance of diary entry representing the entry that is closest 
     # to, but not over, the length that the user could read in the minutes they
     # have available given their reading speed.
+    total_words_to_read = wpm * minutes
+    entries_array = @diary_entries
+
+    entries_array.each_with_index do |entry, index|
+      if entry.count_words > total_words_to_read
+        entries_array.delete[index]
+      end
+    end
+
+
+    current_selection = @diary_entries[1]
+
+    @diary_entries.each { |entry| if remainder of => entry.reading_time < current_selection then current_selection = entry}
+
   end
 end
--- Test Drive A Class System/Exercise/spec/diary_integration_spec.rb
+++ Test Drive A Class System/Exercise/spec/diary_integration_spec.rb
@@ -24,4 +24,39 @@ RSpec.describe Diary do
       expect(diary.count_words).to eq 11
     end
   end
+
+  context "given several entries and a reading speed" do
+    it "returns how long to read all words across all entries" do
+      diary = Diary.new
+      entry1 = DiaryEntry.new("1", "My life is great mate.")
+      entry2 = DiaryEntry.new("2", "My life is now crap.")
+      diary.add(entry1)
+      diary.add(entry2)
+      expect(diary.reading_time(2)).to eq 6
+    end
+  end
+
+  context "given several entries and a reading speed" do
+    it "returns how long to read all words across all entries" do
+      diary = Diary.new
+      entry1 = DiaryEntry.new("1", "My life is great mate.")
+      entry2 = DiaryEntry.new("2", "My life is now crap dude.")
+      diary.add(entry1)
+      diary.add(entry2)
+      expect(diary.reading_time(2)).to eq 7
+    end
+  end
+
+  context "given a reading speed and time available" do
+    it "returns a suitable diary entry for reading" do
+      diary = Diary.new
+      entry1 = DiaryEntry.new("1", "My life is great mate.")
+      entry2 = DiaryEntry.new("2", "My life is now crap because I have lost all my money and my friends abandonded me, isn't this terrible.")
+      entry3 = DiaryEntry.new("3", "My life is now crap because I have lost all my money and my friends abandonded me, isn't this terrible. My life is now crap because I have lost all my money and my friends abandonded me, isn't this terrible.")
+      diary.add(entry1)
+      diary.add(entry2)
+      diary.add(entry3)
+      expect(diary.find_best_entry_for_reading_time(20, 1)).to eq entry2
+    end
+  end
 end
